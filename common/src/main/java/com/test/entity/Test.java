55681507010817967ad1fa89b625e1847642c6ec
miaoqiang <815893248@qq.com>
2020-12-14 15:14:08 +0800

    提供测试初版

---
+++ common/src/main/java/com/test/entity/Test.java
@@ -0,0 +1,14 @@
+package com.test.entity;
+
+public class Test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Test setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
